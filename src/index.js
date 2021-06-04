430df5124c10394496371867d171c8ed4263f9e1
Valentin RONTEIX <valentin.ronteix@ashudev.com>
2021-06-04 16:23:38 +0200

    fix: images not appened in all class

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ require('./css/materialize.css')
 require('./css/all.css')
 require('./css/style.css')
 
-const avatar = document.createElement('img')
 const img = require('./img/profile.jpg');
-avatar.src = img
-avatar.classList.add('responsive-img', 'z-depth-1');
-avatar.alt = 'Valentin RONTEIX';
 
 const app = document.querySelectorAll('.avatar')
 
-app.forEach((userItem) => {
-  userItem.append(avatar);
-});
+app.forEach(element => {
+  const avatar = document.createElement('img')
+  avatar.src = img
+  avatar.classList.add('responsive-img', 'z-depth-1');
+  avatar.alt = 'Valentin RONTEIX';
+  element.append(avatar)
+})
+console.log(app)
