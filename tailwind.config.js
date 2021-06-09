a9efe851d6a54e3843130d0b43ccd7a0e515a390
Valentin RONTEIX <valentin.ronteix@pm.me>
2021-06-09 22:32:08 +0200

    feat: CGU

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ['./docs/*html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
